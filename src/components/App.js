42beff578578d63d54882065f09a96e8a90b005e
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-05-24 18:13:10 +0530

    updated v

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 import { useDispatch, useSelector } from "react-redux";
 import { setData } from "./Reducer";
@@ -10,26 +10,33 @@ const App = () => {
   useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response=>response.json())
-    .then(info=>{dispatch(setData(info))})
+    .then(info=>{
+      dispatch(setData(info))
+      setLoading(false) })
     .catch(err=>console.log('error occured',err))
   },[])
+  let [loading,setLoading]=useState(true)
   // console.log(data)
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1 className="heading" >A short Narration of Lorem Ipsum</h1>
+        <h1 className="heading" >A short Naration of Lorem Ipsum</h1>
         <p className="subHeading" ><b>Below Contains a Tiltle and a Body gotten from a <br />
         random API, Please take your time to Review</b></p>
-        <div className="cardsContainer">
+        {loading &&  <h4>Loading...</h4> }
         {
-          data.map((dta)=>(
-            <section key={dta.id} >
-              <p><b>Title :</b>{dta.title}</p>
-              <p><b>Body :</b>{dta.body}</p>
-            </section>
-          ))
+          !loading &&
+          <ul className="cardsContainer">
+            {
+              data.map((dta) => (
+                <li key={dta.id} className="card">
+                   <h4 className="label">Title:</h4><p className="content">{dta.title}</p>
+                   <h4 className="label">Body:</h4><p className="content">{dta.body}</p>
+                </li>
+              ))
+            }
+          </ul>
         }
-        </div>
     </div>
   )
 }
