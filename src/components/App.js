2be3ab2c289962036ed52dbb50da29804804dc30
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-05-24 18:41:06 +0530

    updated v

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,50 @@
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './../styles/App.css';
-import { useDispatch, useSelector } from "react-redux";
-import { setData } from "./Reducer";
+// import Menu from "./menu.js";
+// import SmallBox from "./innerDisplay.js";
 
-const App = () => {
-  const dispatch=useDispatch()
-  const data=useSelector((state)=>state.data.items)
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>response.json())
-    .then(info=>{
-      dispatch(setData(info))
-      setTimeout(() => setLoading(false), 300) })
-    .catch(err=>console.log('error occured',err))
-  },[dispatch])
-  let [loading,setLoading]=useState(true)
-  // console.log(data)
-  return (
-    <div>
-        {/* Do not remove the main div */}
-        <h1 className="heading" >A short Naration of Lorem Ipsum</h1>
-         
-        <p className="subHeading" ><b>Below Contains A title and Body gotten froma random API, Please take your time to Review</b></p>
-        {loading &&  <h4>Loading...</h4> }
-        {
-          !loading &&
-          <ul className="cardsContainer">
-            {
-              data.map((dta) => (
-                <li key={dta.id} className="card">
-                   <h4  >Title :</h4>  
-              <p className="title" >{dta.title}</p>
+class APP extends React.Component {
+  componentWillUpdate() {
+    try {
+      if (this.props.Begin === true) this.Time();
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-              <h4>Body :</h4>  
-              <p className="body" >{dta.body}</p>
-                </li>
-              ))
-            }
-          </ul>
-        }
-    </div>
-  )
+  render() {
+    const {
+      Begin,
+      cards,
+      Timer,
+      WhenClick,
+      WhenReset,
+      Collect,
+      Update
+    } = this.props;
+    console.log(Update, Begin, Timer);
+    return (
+      <div>
+        <div>
+          <Menu timer={Timer} WhenReset={WhenReset} />
+        </div>
+
+        <div className="main_game">
+          {cards.map((value, index) => (
+            <SmallBox
+              key={index}
+              src={value.src}
+              index={index}
+              id={value.name}
+              displayer={value.Display}
+              WhenClick={WhenClick}
+              Collect={Collect}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default APP;
--- src/components/Store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import reducerSlice from "./Reducer";
-export default configureStore({
-    reducer:{
-        data:reducerSlice
-    }
-})
